b60b5bc2dfa4ca98887c95e3195784f7328b2219
John Correa <ema8720@gmail.com>
2021-08-07 15:45:51 -0700

    Complete index.js to connect to api and get list of posts.

--- api/app.js
+++ api/app.js
@@ -5,18 +5,30 @@ const app = express();
 const Post = require("./models/posts")
 // this will initialize our object
 const postData = new Post()
-const posts = [{
-        "id": "1581461442206",
-        "title": "This is a New Blog Post",
-        "content": "This is the content! ",
-        "post_image": "uploads/post-image-1581461442199.jpg",
-        "added_date": "1581461442206"
-    }]
 
+// setup on app.js Allow_Control-Access-Origin
+app.use((req, res, next)=> {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next(); // 
+})
 
+// set up another middleware for only uploads
+app.use('/uploads', express.static('uploads'))
 
 app.get("/api/posts", (req, res) => {
-    res.status(200).send(posts);
+    res.status(200).send(postData.get());  
+})
+
+
+app.get("/api/posts/:post_id", (req, res) => {
+    const postId = req.params.post_id;
+    // console.log(postId);
+    const foundPost = postData.getIndividualBlog(postId)
+    if (foundPost) {
+        res.status(200).send(foundPost)
+    } else {
+        res.status(404).send("Post not found")
+    }
 })
 
 // Here we are initializing the server
--- api/models/posts.js
+++ api/models/posts.js
@@ -1,22 +1,38 @@
 // create a path
 const PATH = "./data.json"
-
+const fs = require("fs");
 
 class Post {
     get() {
-        // Get Posts
+        // Get Post
+        return this.readData()
     }
 
-    getIndividualBlog() {
+    getIndividualBlog(postId) {
         // Get One Blog Post
+        const posts = this.readData()
+        const foundPost = posts.find((post) => 
+             post.id === postId
+             
+        )
+        return foundPost
+        
     }
-    add() {
+    add(newPost) {
         // Add
-
+        const currentPost = this.readData();
+        currentPost.unshift(newPost);
+        this.storeData(currentPost);
     }
 
     readData() {
-        // 
+        let rawData = fs.readFileSync(PATH)
+        let posts = JSON.parse(rawData);
+        return posts
+    }
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 
 }
